1acb830b5cbe3a641a650e3376c8af6262b5fc24
DMelisena <demonlordx777@gmail.com>
2023-06-04 15:54:21 +0700

    Penambahan Keterangan dan merapikan

--- Skripsi.py
+++ Skripsi.py
@@ -1,22 +1,31 @@
 import math
 from math import *
+
+#print(W)
+
+
+#========Pembatas Dosis
+brp=20/2/50#b atas radiasi pekerja, (20mSv/tahun)*setengah/50 minggu/tahun
+brm=1/2/50#batas radiasi masyarakat, 1mSv/tahun*setengah/50 minggu/tahun
+
+#========Beban Kerja
 pasienperhari=70
 gyperpasien=2
 hariperminggu=5
-W=hariperminggu*gyperpasien*pasienperhari*1000
-#print(W)
 
-brp=20/2/50#b atas radiasi pekerja, (20mSv/tahun)*setengah/50 minggu/tahun
-brm=1/2/50#batas radiasi masyarakat
-#print(brp)
-#print(brm)
-#dsadf=4.15
+#======== Variabel Asumsi Konstan
+W=hariperminggu*gyperpasien*pasienperhari*1000
 U=0.33
 Tf=1
+
+#======== Variabel TVL
 TVL=389
 HVL=TVL*log10(2)
 print("HVL=",HVL)
+
+#======== Variabel tambahan untuk instantenous Dose Rate
 DR=2200*60/100#2200MU/min adalah dose rate maksimum pada LINAC. (1MU=1cGy)
+
 def WeekDR(br,dsadf):
     b=(br*dsadf**2)/(W*U*Tf)
     n=-log(b,10)
@@ -24,6 +33,7 @@ def WeekDR(br,dsadf):
 
 def InstDR(br,dsadf):
     return (br/40/1000)*(dsadf**2)/DR #Dibagi 1000 karenna dirubah dari mSv ke Sv
+
 print ("Barrier primer =", WeekDR(brp,4.15)) 
 print ("Barrier primer+2HVL =", WeekDR(brp,4.15)+(2*HVL)) #WeekDRF
 print (InstDR(brp,4.15)) #InstDRF
