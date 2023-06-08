34812d520fd80fb5f777ba01e29e8c44f91b1820
DMelisena <demonlordx777@gmail.com>
2023-06-08 19:04:08 +0700

    penambahan keterangan

--- primer.py
+++ primer.py
@@ -9,21 +9,21 @@ hariperminggu=5
 
 #======== Variabel Asumsi Konstan
 W=hariperminggu*gyperpasien*pasienperhari*1000
-U=0.33
-Tf=1
+U=0.33 #Faktor Guna dapet dari mana?
 
-#========Pembatas Dosis
-brp=20/2/50#b atas radiasi pekerja, (20mSv/tahun)*setengah/50 minggu/tahun
-brm=1/2/50#batas radiasi masyarakat, 1mSv/tahun*setengah/50 minggu/tahun
+#========Pembatas Dosis======
+#Dikali setengah agar aman menurut ncrp
+brp=20/2/50  #batas radiasi pekerja,   (20mSv/tahun)*setengah/50 minggu/tahun
+brm=1/2/50   #batas radiasi masyarakat,(1mSv/tahun*setengah/50 minggu/tahun
 
-#======== Variabel TVL
+#======== Variabel TVL =========
 TVL=389
-HVL=TVL*log10(2)
+HVL=TVL*log10(2) #Pengubangan dari agar menjadi setengah, menjadi 1/10 laju dosis radiasi
 print("HVL=",HVL)
 
 #======== Variabel tambahan untuk instantenous Dose Rate
 DR=2200*60/100#2200MU/min adalah dose rate maksimum pada LINAC. (1MU=1cGy)
-
+#2200 MU/min = 2200cGy/min = 132000 cGy/jam = 1320 Gy/jam
 def WeekDR(br,dsadf,T):
     b=(br*dsadf**2)/(W*U*T)
     n=-log10(b)
