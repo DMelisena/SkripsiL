5c0069577ff8306c87cf1777096d30a76b4a56aa
DMelisena <demonlordx777@gmail.com>
2023-06-11 10:31:37 +0700

    Penyelesaian fungsi fraction scatter 10 MeV

--- RSHSSekunder.py
+++ RSHSSekunder.py
@@ -26,19 +26,26 @@ def atandeg(dsec,dsca):
 #print("atan =",atanrad(3.15,1)) #Mengecek nilai atan
 #print("Degree = ",atandeg(3.15,1)) #mengecek sudut yang dihasilkan berdasarkan nilai atan yang sebelumnya ditemukan
 
-x1, y1 = 60, 0.000746
-x2, y2 = 90, 0.000381
+#Scatter Fraction sudut x pada energi 10MV
+x1, y1 = 60, 0.000746 #60 derajat
+x2, y2 = 90, 0.000381 #90 derajat
 
 #========Mencari Fungsi ax+b========
 #Data berdasarkan sudut dan scatter fraction pada energi 10MV
+# cari slope
 slope = (y2 - y1) / (x2 - x1)
+# cari intercept
+intercept = y1 - slope *x1
 
-# Calculate y-intercept
-intercept = y1 - slope * x1
+#fungsi ax+b
+print(f"Fungsi y = {slope}x +{intercept}")
 
-# Print the interpolation function
-print(f"Maka diketahui fungsi y = {slope}x + {intercept}")
+#print("Nilai a dari dsec 3.15",atandeg(3.15,1)*slope+intercept)
+def a(dsec):
+    degree = atandeg(dsec,dsca)
+    return slope*degree+intercept
 
+print(f"Nilai scatter fraction(a) pada dsec 3.15 = {a(3.15)}")
 
 def scatter(P,dsec,a,T): # (dsec = jarak pasien ke titik pengukuran ; a= Fraksi hambur atau serapan dosis berkas primer yang terhambur dari pasien)
     B=(P*dsca**2*dsec**2*400)/(a*W*T*F)
