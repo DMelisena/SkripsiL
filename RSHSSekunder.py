40718e5d91cdc45867b91f8b26b47495a8571b77
DMelisena <demonlordx777@gmail.com>
2023-06-27 17:50:19 +0700

    Penambahan slope dan intercept scatter 30 45

--- RSHSSekunder.py
+++ RSHSSekunder.py
@@ -36,17 +36,22 @@ print("atandeg 2705,2025 = ",atandeg(2705,2025))
 #print("Degree = ",atandeg(3.15,1)) #mengecek sudut yang dihasilkan berdasarkan nilai atan yang sebelumnya ditemukan
 
 #Scatter Fraction sudut x pada energi 10MV
-x1, y1 = 60, 0.000746 #60 derajat
-x2, y2 = 90, 0.000381 #90 derajat
+x90, y90 = 90, 0.000381 #90 derajat
+x60, y60 = 60, 0.000746 #60 derajat
+x45, y45 = 45, 0.00135
+x30, y30 = 30, 0.00318
 
 #========Mencari Fungsi ax+b========
 #Data berdasarkan sudut dan scatter fraction pada energi 10MV
-# cari slope
-slope = (y2 - y1) / (x2 - x1) ; print("slope = ",slope)
+#
+############# cari slope 60 90 ###############
+slope = (y90 - y60) / (x90 - x60) ; print("slope 9060 = ",slope)
+slope2 = (y45 - y30) / (x45 - x30); print("slope 4530 = ",slope2)
 # cari intercept
-intercept = y1 - slope *x1 ; print("intercept = ",intercept)
+intercept = y60 - slope *x60 ; print("intercept = ",intercept)
 #fungsi ax+b
 print(f"Fungsi y = {slope}x +{intercept}")
+############# cari slope 60 90 ###############
 
 
 ########## Karena nilai slopenya dah ketemu, degree tinggal dikaliin slope + intercept
