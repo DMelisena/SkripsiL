4297bc94875f8c7d35a0a19b436aba1ae40e5185
DMelisena <demonlordx777@gmail.com>
2023-07-04 12:43:15 +0700

    Automatisasi dan penurunan jumlah angka dibelakang koma

--- RSHSPrimer.py
+++ RSHSPrimer.py
@@ -41,15 +41,16 @@ arrish=["inst \nShield"]
 def WeekDR( P , dsad , T ): #W dan U dah ada, dsad ini di ncrp dpri, jarak dari xray target ke titik perlindungan, kok rasanya beda ama dsad yang kuitung ya?
     arrdsad.append(dsad)
     b=(P * dsad **2 ) / (W *U *T )
-    arrwb.append(b)
+    arrwb.append("%.5f"%b)
     n=-log10(b)
-    arrwn.append(n)
+    arrwn.append("%.5f"%n)
     #return n*TVL #return TVL1+(n-1)TVLe
     sh=TVL1+(TVLe*(n-1))
-    arrwsh.append(sh)
-    arrwshvl.append(sh+2*HVL)
+    arrwsh.append("%.5f"%sh)
+    shhvl=sh+2*HVL
+    arrwshvl.append("%.5f"%shhvl)
     return sh
-
+"""
 def bWeekDR( P , dsad , T ): #W dan U dah ada, dsad ini di ncrp dpri, jarak dari xray target ke titik perlindungan, kok rasanya beda ama dsad yang kuitung ya?
     b=(P * dsad **2 ) / (W *U *T )
 
@@ -62,18 +63,18 @@ def nWeekDR( P , dsad , T ): #W dan U dah ada, dsad ini di ncrp dpri, jarak dari
 
     #return n*TVL #return TVL1+(n-1)TVLe
     return n
-
+"""
 
 def InstDR(P,dsad):
     b=(P/40/1000)*(dsad**2)/DR #Dibagi 1000 karena dirubah dari mSv ke Sv
     arrib.append(b)
     n=-log10(b)
-    arrin.append(n)
+    arrin.append("%.5f"%n)
     #return n*TVL 
     sh =TVL1+(TVLe*(n-1)) 
-    arrish.append(sh)
+    arrish.append("%.5f"%sh)
     return sh
-
+"""
 def bInstDR(P,dsad):
     b=(P/40/1000)*(dsad**2)/DR #Dibagi 1000 karena dirubah dari mSv ke Sv
     print("$$B_{IDR}=\ frac {",P,"(",dsad,"^{2})}{",DR,"}=",b,"$$") #tinggal dihapus jarak \ dan f jadi langsung latex
@@ -104,11 +105,19 @@ data=[
       InstDR(brp,8.04),
       InstDR(brp,8.04)+(2*HVL)],
         ]
+
 #Nilai dsad 7.32 = 1 + 3.240 + 3.080
 
 head=["Dinding", "Bpri","n","WeekB", "WeekB+2HVL","bInstB","nInstB","InstB","InstB+2HVL"]
 print(tabulate(data,headers=head,tablefmt="grid"))
+"""
 
+def primer(P,dsad,T):
+    InstDR(P,dsad)
+    WeekDR(P,dsad,T)
+
+primer(brp,8.04,1)
+primer(brp,8.04,1)
 
 array=[]
 array.append(arrdsad)
