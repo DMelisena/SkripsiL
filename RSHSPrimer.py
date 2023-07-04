9a8c7df7dd61188bb9158b24f07f23f3fcc90dda
DMelisena <demonlordx777@gmail.com>
2023-07-04 11:54:18 +0700

    Pembuatan tabel otomatis, tapi belum fungsi dan koma masih belum dibatasi

--- RSHSPrimer.py
+++ RSHSPrimer.py
@@ -1,6 +1,7 @@
 import math
 from math import *
 from tabulate import tabulate
+import numpy as np
 
 #========Beban Kerja
 pasienperhari=70
@@ -26,12 +27,28 @@ TVL1=0.410 ; TVLe=0.370 #mm
 #========Instantenous Dose Rate=====
 DR=2200*60/100 #2200MU/min *jam/menit /100Gy/(MU=cGy)
 #Apakah MU pada linac itu maksimalnya juga 2200?
-def WeekDR( P , dsad , T ): #W dan U dah ada, dsad ini di ncrp dpri, jarak dari xray target ke titik perlindungan, kok rasanya beda ama dsad yang kuitung ya?
-    b=(P * dsad **2 ) / (W *U *T )
-    n=-log10(b)
 
+arrdsad=["d+SAD"]
+arrwb=["Weekly B"]
+arrwn=["Weekly n"]
+arrwsh=["Weekly \nShield"]
+arrwshvl=["+2HVL"]
+arrib=["Inst B"]
+arrin=["Inst n"]
+arrish=["inst \nShield"]
+
+
+def WeekDR( P , dsad , T ): #W dan U dah ada, dsad ini di ncrp dpri, jarak dari xray target ke titik perlindungan, kok rasanya beda ama dsad yang kuitung ya?
+    arrdsad.append(dsad)
+    b=(P * dsad **2 ) / (W *U *T )
+    arrwb.append(b)
+    n=-log10(b)
+    arrwn.append(n)
     #return n*TVL #return TVL1+(n-1)TVLe
-    return TVL1+(TVLe*(n-1))
+    sh=TVL1+(TVLe*(n-1))
+    arrwsh.append(sh)
+    arrwshvl.append(sh+2*HVL)
+    return sh
 
 def bWeekDR( P , dsad , T ): #W dan U dah ada, dsad ini di ncrp dpri, jarak dari xray target ke titik perlindungan, kok rasanya beda ama dsad yang kuitung ya?
     b=(P * dsad **2 ) / (W *U *T )
@@ -49,9 +66,14 @@ def nWeekDR( P , dsad , T ): #W dan U dah ada, dsad ini di ncrp dpri, jarak dari
 
 def InstDR(P,dsad):
     b=(P/40/1000)*(dsad**2)/DR #Dibagi 1000 karena dirubah dari mSv ke Sv
+    arrib.append(b)
     n=-log10(b)
+    arrin.append(n)
     #return n*TVL 
-    return TVL1+(TVLe*(n-1)) 
+    sh =TVL1+(TVLe*(n-1)) 
+    arrish.append(sh)
+    return sh
+
 def bInstDR(P,dsad):
     b=(P/40/1000)*(dsad**2)/DR #Dibagi 1000 karena dirubah dari mSv ke Sv
     print("$$B_{IDR}=\ frac {",P,"(",dsad,"^{2})}{",DR,"}=",b,"$$") #tinggal dihapus jarak \ dan f jadi langsung latex
@@ -86,3 +108,27 @@ data=[
 
 head=["Dinding", "Bpri","n","WeekB", "WeekB+2HVL","bInstB","nInstB","InstB","InstB+2HVL"]
 print(tabulate(data,headers=head,tablefmt="grid"))
+
+
+array=[]
+array.append(arrdsad)
+array.append(arrwb)
+array.append(arrwn)
+array.append(arrwsh)
+array.append(arrwshvl)
+array.append(arrib)
+array.append(arrin)
+array.append(arrish)
+
+"""
+array=np.array(array)
+print("Data type:", array.dtype)
+tarray=np.transpose(array)
+print(tabulate(tarray,tablefmt="grid"))
+"""
+nparray=np.array(array)
+obarray=np.array(nparray,dtype=object)
+tarray=obarray.T
+trarray=np.transpose(obarray)
+print("Data type:", trarray.dtype)
+print(tabulate(trarray,tablefmt="grid"))
