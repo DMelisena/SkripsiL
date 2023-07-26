870da05f3223e85df73ff939be8d965b77223e68
DMelisena <demonlordx777@gmail.com>
2023-07-26 21:14:07 +0700

    pembuatan variabel untuk penghitungan laju dosis

--- rshs/logdoseplot.py
+++ rshs/logdoseplot.py
@@ -2,14 +2,18 @@ import openmc
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-sp = openmc.StatePoint('./statepoint.11.h5')
+sp = openmc.StatePoint('./statepoint.5.h5')
 meshtally = sp.tallies[1]
 dosevalues = meshtally.get_values()
-
-dosevalues.shape = (100,100)
-
+x=500 #harus sama dengan resolusi pada file utama
+y=500
+dosevalues.shape = (x,y)
+s_rate=4.87805e7
+v=(2000/x)*(2000/y)*300
+dosevalues = dosevalues*s_rate/v #picosieverts/s
+dose=dosevalues*1000*3600 #microsieverts/hour
 fig, ax = plt.subplots()
-cs = ax.imshow(dosevalues, cmap='coolwarm', norm=LogNorm()) # type: ignore
+cs = ax.imshow(dose, cmap='coolwarm', norm=LogNorm()) # type: ignore
 cb = plt.colorbar(cs)
 ax.set_title('Power density (kW/cm$^3$)') #type: ignore
 # plt.axis('off')
@@ -18,3 +22,4 @@ plt.savefig('figlogdoseplot.png')
 plt.show()
 # plt.close()
 # plt.clf()
+
