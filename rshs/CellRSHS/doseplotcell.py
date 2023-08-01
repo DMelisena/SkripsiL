510081fe0ec56532d6450cd0def36ce209137401
DMelisena <demonlordx777@gmail.com>
2023-08-01 13:06:21 +0700

    Pengubahan nilai s_rate karena kesalahan konversi, hasil masih sama

--- rshs/CellRSHS/doseplotcell.py
+++ rshs/CellRSHS/doseplotcell.py
@@ -14,17 +14,22 @@ meshtally = sp.tallies[1]
 x=500#harus sama dengan resolusi pada file utama
 y=500
 #s_rate=4.87805e7 #source rate ICRP 116
-s_rate=34.52580998
+#s_rate=34.52580998
+#dose=dosevalues*1000*3600 #microsieverts/hour
+
+s_rate=34525809.98
 v=(2000/x)*(2000/y)*300 #volume of room dose distribution
 dosevalues=meshtally.get_values()
 dosevalues.shape=(x,y)
 dosevalues = dosevalues*s_rate/v #picosieverts/s
-dose=dosevalues*1000*3600 #microsieverts/hour
+dose=(dosevalues/1000000)*3600 #microsieverts/hour
+
+
 fig, ax = plt.subplots()
 cs = ax.imshow(dose, cmap='coolwarm', norm=LogNorm()) # type: ignore
 cb = plt.colorbar(cs)
 ax.set_title('Power density (kW/cm$^3$)') #type: ignore
-plt.savefig('RoomDoseDistribution.png',dpi=3000)
+#plt.savefig('RoomDoseDistribution.png',dpi=3000 )
 # plt.axis('off')
 
 
@@ -37,7 +42,7 @@ celldosestddev.shape = celldosestddev.shape[0]
 
 vcell=10.8*50*200
 
-celldosevalues = celldosevalues * s_rate / vcell *1000*3600
+celldosevalues = celldosevalues * s_rate / vcell /1000000*3600
 celldosestddev = celldosestddev * s_rate / vcell
 
 for v,s in zip(celldosevalues,celldosestddev):
@@ -56,7 +61,7 @@ phandosestddev.shape = phandosestddev.shape[0]
 
 vcell=5*40*40
 
-phandosevalues = phandosevalues * s_rate / vcell *1000*3600
+phandosevalues = phandosevalues * s_rate / vcell /1000000*3600
 phandosestddev = phandosestddev * s_rate / vcell
 
 for v,s in zip(phandosevalues,phandosestddev):
