fd8bc823bb97c878b262c8ef24dae7ab6817d3f1
DMelisena <demonlordx777@gmail.com>
2023-08-02 18:18:16 +0700

    Penambahan Eksekusi terpisah untuk masing-masing fungsi

---
+++ rshs/CellRSHS/270degree/plot270.py
@@ -0,0 +1,85 @@
+import openmc
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+sp = openmc.StatePoint('./statepoint.5.h5')
+
+f=open("output.txt","w")
+f.write(str(sp.tallies))
+f.close()
+
+meshtally = sp.tallies[1]
+
+
+x=500#harus sama dengan resolusi pada file utama
+y=500
+#s_rate=4.87805e7 #source rate ICRP 116
+#s_rate=34.52580998
+#dose=dosevalues*1000*3600 #microsieverts/hour
+
+s_rate=34525809.98
+v=(2000/x)*(2000/y)*300 #volume of room dose distribution
+dosevalues=meshtally.get_values()
+dosevalues.shape=(x,y)
+dosevalues = dosevalues*s_rate/v #picosieverts/s
+dose=(dosevalues/1000000)*3600 #microsieverts/hour
+
+
+fig, ax = plt.subplots()
+cs = ax.imshow(dose, cmap='coolwarm', norm=LogNorm()) # type: ignore
+cb = plt.colorbar(cs)
+ax.set_title('Power density (kW/cm$^3$)') #type: ignore
+#plt.savefig('RoomDoseDistribution.png',dpi=3000 )
+# plt.axis('off')
+
+
+celltally = sp.tallies[2]
+celldosevalues = celltally.get_values()
+celldosestddev = celltally.std_dev 
+
+celldosevalues.shape = celldosevalues.shape[0]
+celldosestddev.shape = celldosestddev.shape[0]
+
+vcell=10.8*50*200
+
+celldosevalues = celldosevalues * s_rate / vcell /1000000*3600
+celldosestddev = celldosestddev * s_rate / vcell
+
+for v,s in zip(celldosevalues,celldosestddev):
+    f=open("output.txt","a")
+    print(f'{v} +- {s}')
+    f.write(str(f'\n{v} +- {s}'))
+    f.close()
+    
+
+phantally = sp.tallies[3]
+phandosevalues = phantally.get_values()
+phandosestddev = phantally.std_dev 
+
+phandosevalues.shape = phandosevalues.shape[0]
+phandosestddev.shape = phandosestddev.shape[0]
+
+vcell=5*40*40
+
+#phandosevalues = phandosevalues * s_rate / vcell /1000000*3600
+phandosestddev = phandosestddev * s_rate / vcell
+
+for v,s in zip(phandosevalues,phandosestddev):
+    f=open("output.txt","a")
+    print(f'{v} +- {s}')
+    f.write(str(f'\n{v} +- {s}'))
+    f.close()
+
+plt.show()
+#dosevalues = dosevalues*s_rate/v #picosieverts/s
+
+"""
+# plt.show()
+plt.savefig('figlogdoseplot.png')
+plt.show()
+# plt.close()
+# plt.clf()
+"""
+
+
+# plt.clf()
