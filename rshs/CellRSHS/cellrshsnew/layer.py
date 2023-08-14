eb30bd912478b9476ccc94c093a898fbae12cf22
DMelisena <demonlordx777@gmail.com>
2023-08-14 21:25:20 +0700

    Penyelesaian Fungsi loop 50 tally, tapi belum di exec

--- rshs/CellRSHS/cellrshsnew/layer.py
+++ rshs/CellRSHS/cellrshsnew/layer.py
@@ -36,7 +36,7 @@ for i in range(1,51):  #
     #X Plane Maker
     varname = f"detaxz{i}"  # Generate variable name
     varval = f"openmc.XPlane(-2.5+{i*0.1},boundary_type='transmission')"  # Define the calculation expression 
-    varnamearr.append(varname)
+    varnamearr.append(varname)# type: ignore
     varvalarr.append(varval)
     # Create the variable dynamically using exec()
     exec(f"{varname} = {varval}")
@@ -58,3 +58,26 @@ for i in range (0,50):
     print(f"test :{varnamearr[i]} = {(varvalarr[i])}")
     print(f"test :{geoarr[i]} = {(geovalarr[i])}")
     print(f"test :{cellarr[i]} = {(cellvalarr[i])}")
+#univ=openmc.universe(cells=[])
+#univ.cells.append({detaxcell{i}})
+for i in range (1,51):
+    #exec(f"univ.cells.append(detaxcell{i})")
+    print(f"univ.cells.append(detaxcell{i})")
+
+
+#Tally Water Phantom
+s="wphantom_cell=openmc.CellFilter("
+for i in range (1,51):
+    s+=f"detaxcell{i},"
+s+=")"
+#exec(s)
+print(s)
+tally3=openmc.Tally(name='wphantom')
+particle3= openmc.ParticleFilter('photon')
+#tally3.filters=[wphantom_cell,particle3]
+#Energy_filter = openmc.EnergyFilter([1e-3, 1e13])
+tally3.filters=[wphantom_cell,particle3,dose_filter]  #output pSvcm3/src 
+tally3.scores = ['flux']
+tally.append(tally3)
+tally.export_to_xml()
+openmc.run()
