4cb02484472757b7143b454a5699a8606a08c53e
DMelisena <demonlordx777@gmail.com>
2023-08-14 20:50:19 +0700

    Penyelesaian plane dan volume cell, kurang fill, tambahin ke universe dan setting sebagai tally

--- rshs/CellRSHS/cellrshsnew/layer.py
+++ rshs/CellRSHS/cellrshsnew/layer.py
@@ -1,4 +1,12 @@
 import openmc
+#Perlu membuat geometri dan cell secara otomatis
+# 1. Declare plane geometri
+#Harapannya agar terbuat detaxz1=-2.4, detaxz2=-2.3, dst
+# 2. Declare geometri volume cell
+#detax1-45
+# 3. Declare cell
+# 4. Masukin ke Universe
+# 5. Masukin ke Tally
 
 detaxu=openmc.YPlane(5,boundary_type='transmission')
 detaxs=openmc.YPlane(-5,boundary_type='transmission')
@@ -15,7 +23,7 @@ geoarr=[]
 geovalarr=[]
 
 
-for i in range(1,50):  # Loop from 3 to 4 (inclusive)
+for i in range(1,51):  # Loop from 1 to 50 (inclusive)
     varname = f"detaxz{i}"  # Generate variable name
     varval = f"openmc.XPlane(-2.5+{i*0.1},boundary_type='transmission')"  # Define the calculation expression 
     varnamearr.append(varname)
@@ -29,6 +37,7 @@ for i in range(1,50):  # Loop from 3 to 4 (inclusive)
     geovalarr.append(geoval)
     exec(f"{geoname} = {geoval}")
 
-for i in range (3,39):
-    print(f"{varnamearr[i]} = {(varvalarr[i])}")
-    print(f"{geoarr[i]} = {(geovalarr[i])}")
+
+for i in range (0,50):
+    print(f"test :{varnamearr[i]} = {(varvalarr[i])}")
+    print(f"test :{geoarr[i]} = {(geovalarr[i])}")
