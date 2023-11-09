0cae420340ebf4aad6730eb0cfabf1812c17c078
DMelisena <demonlordx777@gmail.com>
2023-11-09 18:03:54 +0700

    Pembenaran Fungsi doseplotcell

--- 2simulasi/Kalibrasi/Old Callibration/doseplotcell.py
+++ 2simulasi/Kalibrasi/Old Callibration/doseplotcell.py
@@ -14,28 +14,7 @@ print(sp.tallies[2])
 print(sp.tallies[3])
 
 
-x=500#harus sama dengan resolusi pada file utama
-y=500
-#s_rate=4.87805e7 #source rate ICRP 116
-#s_rate=34.52580998
-#dose=dosevalues*1000*3600 #microsieverts/hour
-
-######## Pembuatan Visualisasi Distribusi Dosis #######
-s_rate=1.29610389e+18
-v=(2000/x)*(2000/y)*300 #volume of room dose distribution
-dosevalues=meshtally.get_values()
-dosevalues.shape=(x,y)
-dosevalues = dosevalues*s_rate/v #picosieverts/s
-dose=(dosevalues/1000000)*3600 #microsieverts/hour
-
-
-fig, ax = plt.subplots()
-cs = ax.imshow(dose, cmap='coolwarm', norm=LogNorm()) # type: ignore
-cb = plt.colorbar(cs)
-ax.set_title('Power density (kW/cm$^3$)') #type: ignore
-plt.savefig('RoomDoseDistribution.png',dpi=900 )
-plt.axis('off')
-######## Hasil S_rate Simulasi #########################
+######## Hasil S_rate Simulasi ##################################
 
 phantally = sp.tallies[3]
 phandosevalues = phantally.get_values()#pSvcm3/src
@@ -58,8 +37,36 @@ print( "\n========== 600 MU/min sesuai S_rate? ============")
 print(f"source rate *phantomdosevaluse/v cell axis= mu\n{s_rate}x{phandosevalues}/{axvcell}={mu}\n={phandosevalues*s_rate/axvcell} pSv/s")
 print(f"={phandosevalues*s_rate/axvcell*60/1e10} MU\n")#pSv/sec*(src/s)/cm3
 #phandosevalues = phandosevalues * s_rate / axvcell /1000000*3600 #
-#phandosestddev = phandosestddev * s_rate / axvcell
-##################################################################
+
+#################################################################
+
+
+######## Pembuatan Visualisasi Distribusi Dosis #################
+
+x=500#harus sama dengan resolusi pada file utama
+y=500
+#s_rate=4.87805e7 #source rate ICRP 116
+#s_rate=34.52580998
+#dose=dosevalues*1000*3600 #microsieverts/hour
+
+
+v=(2000/x)*(2000/y)*300 #volume of room dose distribution
+dosevalues=meshtally.get_values() #Nilai perpixel dari grid 500x500
+dosevalues.shape=(x,y)
+#pSvcm3/src*(src/s)/cm3=pSv/s
+dosevalues = dosevalues*s_rate/v #pSv/s
+dose=(dosevalues/1_000_000)*3600 #pSv/s -> uSv/hour
+
+fig, ax = plt.subplots()
+cs = ax.imshow(dose, cmap='coolwarm', norm=LogNorm()) # type: ignore
+cb = plt.colorbar(cs)
+ax.set_title('Distribusi Dosis Ruangan uSv/hour)') #type: ignore
+plt.savefig('RoomDoseDistribution.png',dpi=900 )
+plt.axis('off')
+
+plt.show()
+#################################################################
+
 
 phandosevalues *= factoruSv
 phandosestddev *= factoruSv
@@ -88,7 +95,6 @@ for v,s in zip(dose,dosestddev):
     f.write(str(f'\n{v} +- {s} uSv/h'))
     f.close()
  
-#plt.show()
 #dosevalues = dosevalues*s_rate/v #picosieverts/s
 
 """
