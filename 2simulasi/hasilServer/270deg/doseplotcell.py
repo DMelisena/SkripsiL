abefc9fb12384334e7f1f91ce217edda383c4a4a
DMelisena <demonlordx777@gmail.com>
2023-11-16 15:36:27 +0700

    perapihan listing output ruangan

--- 2simulasi/hasilServer/270deg/doseplotcell.py
+++ 2simulasi/hasilServer/270deg/doseplotcell.py
@@ -13,49 +13,17 @@ print(sp.tallies[1])
 print(sp.tallies[2])
 #print(sp.tallies[3])
 
-"""
-######## Hasil S_rate Simulasi ##################################
-
-phantally = sp.tallies[3]
-phandosevalues = phantally.get_values()#pSvcm3/src
-phandosestddev = phantally.std_dev 
-
-phandosevalues.shape = phandosevalues.shape[0]
-phandosestddev.shape = phandosestddev.shape[0]
-
-axvcell=5*10*10#cm3
-mu=1e11#600 MU/min = 10 cGy/s = 1e11 pSv/s 
-# Pencarian Faktor Konversi dengan 600 MU/m=1e11pSv/s, volume phantom (cm3) dan dosis simulasi (pSvcm3/src)
-s_rate=mu*axvcell/phandosevalues #src/s
-print( "\n========== Pencarian Nilai S rate ============")
-print("s_rate=mu*axvcell/phandosevalues #src/s")
-print(f"={mu}*{axvcell}/{phandosevalues}={s_rate}\n")
-factorMU = 60/1e10 # pSv/s -> MU/min
-factoruSv = 3600/1e6 # pSv/s -> uSv/h
-
-print( "\n========== 600 MU/min sesuai S_rate? ============")
-print(f"source rate *phantomdosevaluse/v cell axis= mu\n{s_rate}x{phandosevalues}/{axvcell}={mu}\n={phandosevalues*s_rate/axvcell} pSv/s")
-print(f"={phandosevalues*s_rate/axvcell*60/1e10} MU\n")#pSv/sec*(src/s)/cm3
-#phandosevalues = phandosevalues * s_rate / axvcell /1000000*3600 #
-
-#################################################################
-"""
-
 ######## Pembuatan Visualisasi Distribusi Dosis #################
 
 x=500#harus sama dengan resolusi pada file utama
 y=500
-#s_rate=4.87805e7 #source rate ICRP 116
-#s_rate=34.52580998
-#dose=dosevalues*1000*3600 #microsieverts/hour
 
-s_rate=3.293648066139751e+17
-v=(2000/x)*(2000/y)*300 #volume of room dose distribution
+s_rate=3.293648066139751e+17 #src/s
+v=(2000/x)*(2000/y)*1000 #volume of room dose distribution
 dosevalues=meshtally.get_values() #Nilai perpixel dari grid 500x500
 dosevalues.shape=(x,y)
 #pSvcm3/src*(src/s)/cm3=pSv/s
-s_rate=6.967475822858894e+18
-dosevalues = dosevalues*s_rate/v #pSv/s
+dosevalues = dosevalues*s_rate/v #pSv/s = (pSv*cm3/src) *src/s /cm3
 dose=(dosevalues/1_000_000)*3600 #pSv/s -> uSv/hour
 
 fig, ax = plt.subplots()
@@ -67,15 +35,6 @@ plt.axis('off')
 
 #################################################################
 
-"""
-phandosevalues *= factoruSv
-phandosestddev *= factoruSv
-for v,s in zip(phandosevalues,phandosestddev):
-    f=open("output.txt","a")
-    print(f'{v} +- {s}')
-    f.write(str(f'\n{v} +- {s}'))
-    f.close()
-"""
 celltally = sp.tallies[2]
 celldosevalues = celltally.get_values() #pSvcm3/src;dosevolume per source
 celldosestddev = celltally.std_dev 
@@ -83,14 +42,23 @@ print(celltally)
 celldosevalues.shape = celldosevalues.shape[0]
 celldosestddev.shape = celldosestddev.shape[0]
 
-vcell=10.8*50*200
+vcell=10.8*50*200 #cm3
+
+dose_psvs=celldosevalues * s_rate / vcell
+usvh_dose=(dosepsv/1e6)*3600
+
+stddev_psvs=celldosestdev*s_rate/vcell
+usvh_stddev=(stddev_psvs/1e6)*3600
+
+dose=doseusvh
+dosestdev=usvh_stddev
 
 #dose = celldosevalues *s_rate/ vcell #pSvcm3/src * (src/s) / cm3= pSv/s
 #dose=(dose/1e6)*3600 #pSv/s -> uSv/h 3.6e9
 #dosestddev = (celldosestddev * s_rate / vcell) *(1e6/3600) 
-k=2457897.1324533457
-dose=k*celldosevalues*600/vcell
-dosestddev = (k*celldosevalues*600/vcell) *(1e6/3600) 
+#k=2457897.1324533457
+#dose=k*celldosevalues*600/vcell
+#dosestddev = (k*celldosevalues*600/vcell) *(1e6/3600) 
 
 
 print(dose)
