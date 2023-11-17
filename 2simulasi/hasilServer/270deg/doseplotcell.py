e64a56252a34f25b7275b9ea7738c2cac40d19da
DMelisena <demonlordx777@gmail.com>
2023-11-17 18:46:11 +0700

    Penggunaan konversi K pada distribusi ruangan

--- 2simulasi/hasilServer/270deg/doseplotcell.py
+++ 2simulasi/hasilServer/270deg/doseplotcell.py
@@ -19,13 +19,16 @@ x=500#harus sama dengan resolusi pada file utama
 y=500
 
 s_rate=3.293648066139751e+17 #src/s
-v=(2000/x)*(2000/y)*1000 #volume of room dose distribution
+v=(2000/x)*(2000/y)*300#volume of room dose distribution
 dosevalues=meshtally.get_values() #Nilai perpixel dari grid 500x500
 dosevalues.shape=(x,y)
 #pSvcm3/src*(src/s)/cm3=pSv/s
-dosevalues = dosevalues*s_rate/v #pSv/s = (pSv*cm3/src) *src/s /cm3
-dose=(dosevalues/1_000_000)*3600 #pSv/s -> uSv/hour
+#dosevalues = dosevalues*s_rate/v #pSv/s = (pSv*cm3/src) *src/s /cm3
+#dose=(dosevalues/1_000_000)*3600 #pSv/s -> uSv/hour
+#k=116499529.9817541
+k=124276037.76335144
 
+dose = dosevalues*k/v
 fig, ax = plt.subplots()
 cs = ax.imshow(dose, cmap='coolwarm', norm=LogNorm()) # type: ignore
 cb = plt.colorbar(cs)
@@ -44,14 +47,18 @@ celldosestddev.shape = celldosestddev.shape[0]
 
 vcell=10.8*50*200 #cm3
 
-dose_psvs=celldosevalues * s_rate / vcell
-usvh_dose=(dosepsv/1e6)*3600
+#dose_psvs=celldosevalues * s_rate / vcell
+#usvh_dose=(dose_psvs/1e6)*3600
 
-stddev_psvs=celldosestdev*s_rate/vcell
-usvh_stddev=(stddev_psvs/1e6)*3600
+#stddev_psvs=celldosestddev*s_rate/vcell
+#usvh_stddev=(stddev_psvs/1e6)*3600
 
-dose=doseusvh
-dosestdev=usvh_stddev
+#dose=usvh_dose
+#dosestddev=usvh_stddev
+
+dose=celldosevalues*k/vcell
+dosestddev=celldosestddev*k/vcell
+
 
 #dose = celldosevalues *s_rate/ vcell #pSvcm3/src * (src/s) / cm3= pSv/s
 #dose=(dose/1e6)*3600 #pSv/s -> uSv/h 3.6e9
