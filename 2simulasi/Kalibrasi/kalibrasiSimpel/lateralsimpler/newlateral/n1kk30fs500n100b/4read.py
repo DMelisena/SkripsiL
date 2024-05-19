a391d52b1ab155db5a5fd0615a5a62db8f311d7d
DMelisena <demonlordx777@gmail.com>
2024-05-20 00:53:02 +0700

    Adding the batch to 100

---
+++ 2simulasi/Kalibrasi/kalibrasiSimpel/lateralsimpler/newlateral/n1kk30fs500n100b/4read.py
@@ -0,0 +1,16 @@
+import openmc
+import numpy as np
+
+statepoint = openmc.StatePoint('statepoint.100.h5')
+tallies = statepoint.tallies
+tally = tallies[1]
+
+import matplotlib.pyplot as plt
+
+flux = tally.get_slice(scores=['flux'])
+data = flux.mean.flatten() #untuk mengubah data dari [[][][]],[[][... menjadi [ , , ,... ]
+data_max=np.max(data)
+scaled=data/data_max*100
+x = np.linspace(-25, 25, len(scaled))
+plt.plot(x, data)
+plt.savefig('flux.png')
