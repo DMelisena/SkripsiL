6c856e70f910d14f40f42e8bfc82c2b2fe085838
DMelisena <demonlordx777@gmail.com>
2024-05-20 00:55:27 +0700

    The other geometry, unused the result should be flatter

---
+++ 2simulasi/Kalibrasi/kalibrasiSimpel/lateralsimpler/newlateral/4newlat_.py
@@ -0,0 +1,80 @@
+import openmc, openmc.model, openmc.stats
+
+ROOM_SIZE = 100
+SOURCE_SIZE = 20
+PHANTOM_SIZE = 30
+
+air = openmc.Material(name='air')
+air.set_density('g/cm3', 0.001)
+air.add_element('N', 0.7)
+air.add_element('O', 0.3)
+
+water = openmc.Material(name='water')
+water.set_density('g/cm3', 1.0)
+water.add_element('H', 2)
+water.add_element('O', 1)
+
+materials = openmc.Materials([air, water])
+materials.export_to_xml()
+
+# create the room
+roomX0 = openmc.XPlane(x0=-ROOM_SIZE/2, boundary_type='vacuum')
+roomX1 = openmc.XPlane(x0= ROOM_SIZE/2, boundary_type='vacuum')
+roomY0 = openmc.YPlane(y0=-ROOM_SIZE/2, boundary_type='vacuum')
+roomY1 = openmc.YPlane(y0= ROOM_SIZE/2, boundary_type='vacuum')
+roomZ0 = openmc.ZPlane(z0=-ROOM_SIZE/2, boundary_type='vacuum')
+roomZ1 = openmc.ZPlane(z0= ROOM_SIZE/2, boundary_type='vacuum')
+roomRegion = +roomX0 & -roomX1 & +roomY0 & -roomY1 & +roomZ0 & -roomZ1
+roomCell = openmc.Cell(region=roomRegion, fill=air)
+
+# create the phantom
+phantomX0 = openmc.XPlane(x0=-PHANTOM_SIZE/2)
+phantomX1 = openmc.XPlane(x0= PHANTOM_SIZE/2)
+phantomY0 = openmc.YPlane(y0=-PHANTOM_SIZE/2)
+phantomY1 = openmc.YPlane(y0= PHANTOM_SIZE/2)
+phantomZ0 = openmc.ZPlane(z0=-PHANTOM_SIZE/2)
+phantomZ1 = openmc.ZPlane(z0= PHANTOM_SIZE/2)
+phantomRegion = +phantomX0 & -phantomX1 & +phantomY0 & -phantomY1 & +phantomZ0 & -phantomZ1
+phantomCell = openmc.Cell(region=phantomRegion, fill=water)
+
+universe = openmc.Universe(cells=[roomCell, phantomCell])
+geom = openmc.Geometry(universe)
+geom.export_to_xml()
+
+
+## source
+d = 10
+t = 1
+source = openmc.Source()
+source.space = openmc.stats.Box((-PHANTOM_SIZE/2-d, -SOURCE_SIZE/2, -SOURCE_SIZE/2), (-PHANTOM_SIZE/2-d-t, SOURCE_SIZE/2, SOURCE_SIZE/2))
+source.angle = openmc.stats.Monodirectional((1, 0, 0))
+source.energy = openmc.stats.Discrete([10e6], [1])
+source.particle = 'photon'
+
+## tally
+mesh = openmc.Mesh()
+mesh.dimension = [1, 1, 100]
+mesh.lower_left = [-PHANTOM_SIZE/2, -PHANTOM_SIZE/2, -PHANTOM_SIZE/2] #type: ignore
+mesh.upper_right = [PHANTOM_SIZE/2, PHANTOM_SIZE/2, PHANTOM_SIZE/2] #type: ignore
+mesh_filter = openmc.MeshFilter(mesh)
+
+particle_filter = openmc.ParticleFilter(['photon'])
+
+tally = openmc.Tally()
+tally.filters = [mesh_filter, particle_filter]
+tally.scores = ['flux']
+
+tallies = openmc.Tallies()
+tallies.append(tally)
+tallies.export_to_xml()
+
+## settings
+settings = openmc.Settings()
+settings.run_mode = 'fixed source'
+settings.particles = 100000
+settings.batches = 100
+settings.inactive = 0
+settings.source = source
+settings.export_to_xml()
+
+openmc.run(
