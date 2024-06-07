4764a32924b90a49f6e0593af277cd824c82aa71
DMelisena <demonlordx777@gmail.com>
2024-06-08 04:12:40 +0700

    Adding result for doseplotcell on each degree room simulation

--- 2simulasi/CellRSHS/1kk/180/doseplotcell.py
+++ 2simulasi/CellRSHS/1kk/180/doseplotcell.py
@@ -1,6 +1,7 @@
-import openmc
+import openmc # type: ignore
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+import pandas as pd
 
 sp = openmc.StatePoint('./statepoint.100.h5')
 
@@ -13,13 +14,19 @@ print(sp.tallies[1])
 print(sp.tallies[2])
 #print(sp.tallies[3])
 
-######## Pembuatan Visualisasi Distribusi Dosis #################
-
 x=500#harus sama dengan resolusi pada file utama
 y=500
 
 s_rate=3.293648066139751e+17 #src/s
+cfac=1.0993814207650992e+17 
+# dose * flux * src_rate * t / V = [pSv cm²] [p-cm/src] [src/sec] [sec] / [cm³] = [pSv]
+# dose * cfac *t/V=pSv
+# dose*cfac*60/V
+
+
 v=(2000/x)*(2000/y)*1000 #volume of room dose distribution
+
+# {{{ ######## Pembuatan Visualisasi Distribusi Dosis #################
 dosevalues=meshtally.get_values() #Nilai perpixel dari grid 500x500
 dosevalues.shape=(x,y)
 #pSvcm3/src*(src/s)/cm3=pSv/s
@@ -32,8 +39,8 @@ cb = plt.colorbar(cs)
 ax.set_title('Distribusi Dosis Ruangan (uSv/hour)') #type: ignore
 plt.savefig('RoomDoseDistribution.png',dpi=900 )
 plt.axis('off')
-
 #################################################################
+# }}}
 
 celltally = sp.tallies[2]
 celldosevalues = celltally.get_values() #pSvcm3/src;dosevolume per source
@@ -53,7 +60,13 @@ usvh_stddev=(stddev_psvs/1e6)*3600
 dose=usvh_dose
 dosestddev=usvh_stddev
 
-#dose = celldosevalues *s_rate/ vcell #pSvcm3/src * (src/s) / cm3= pSv/s
+dose= celldosevalues*cfac*60/vcell
+stddev_psvs=celldosestddev*cfac*60/vcell
+
+DF=pd.DataFrame(dose,dosestddev)
+
+DF.to_csv("cellTallyDose.csv")
+
 #dose=(dose/1e6)*3600 #pSv/s -> uSv/h 3.6e9
 #dosestddev = (celldosestddev * s_rate / vcell) *(1e6/3600) 
 #k=2457897.1324533457
