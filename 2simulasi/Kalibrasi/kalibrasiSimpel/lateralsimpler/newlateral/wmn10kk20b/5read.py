3ffc8469770eda693a67e4a77a3cc0622e938305
DMelisena <demonlordx777@gmail.com>
2024-06-12 19:05:23 +0700

    5read Creating src_rate

--- 2simulasi/Kalibrasi/kalibrasiSimpel/lateralsimpler/newlateral/wmn10kk20b/5read.py
+++ 2simulasi/Kalibrasi/kalibrasiSimpel/lateralsimpler/newlateral/wmn10kk20b/5read.py
@@ -173,6 +173,20 @@ x = np.linspace(-25, 25, len(datay))
 
 max_index=np.argmax(datay)
 y_max_con = np.max(datay)
+#TODO: Search the value of flux on dose values
+print("=========================")
+print("search the value of flux")
+print(datay)
+print(np.max(datay))
+print(np.argmax(datay))
+maxdoseadress=np.argmax(datay)
+print(maxdoseadress)
+fluxymax=dosev[maxdoseadress]
+
+print(fluxymax)
+
+print("=========================")
+
 xv=np.linspace(0,30,len(datay))
 ymaxv=np.max(datay)
 yflatv=datay/ymaxv*100
@@ -240,17 +254,26 @@ plt.show()
 #https://openmc.discourse.group/t/openmc-data-dose-coefficients/1634/2
 # 1cGy = 1e10 pSv
 # 1e10pSv = dose * flux * src_rate * t / V
+#
 # 1e10pSv/s = dose * flux * src_rate / V
-#  600 MU/min = 6e12pSv/min
+#  600 MU/min = 600 uSv/min = 6e12pSv/min
+# 6e12pSv/min = dose * flux*src_rate /V
+# 1e11 pSv/s =dose * flux*src_rate /V
+# src_rate= (6e12pSv/min * V)/dose*flux5
+v=50*50*0.1
+volume_wp=(d/n)*6*6
+wpdose=y_max_con
+
+src_rate= 1e11*volume_wp/(wpdose*fluxymax)
+print(f"src_rate = 1e11 pSv/s * (volume_wp) cm3 / ({wpdose}pSv cm² * {fluxymax}p-cm/src) = {src_rate}src/sec")
+print(f"src_rate={src_rate}src/sec")
+
 # 6e12pSv/min = dose * flux*src_rate /V
 # Cfactor = flux*src_rate
 # 6e12pSv/min = dose * Cfac / V
 # cfac = 6e12pSv*V/(dose)
 
 mu=1e11 #600 MU/min = 10 cGy/s = 1e11 pSv/s 
-v=50*50*0.1
-volume_wp=(d/n)*6*6
-wpdose=y_max_con
 #print( f"\ncfactor = 1e11 pSv/sec*{v}cm3/(dose({wpdose}))")
 print( f"\ncfac= {1e11*v/wpdose}")
 print( f"cres=cfac*cconv= {1e11*v/wpdose}*{3600/1e6} = ",1e11*v/wpdose*(3600/1e6),"\n")
@@ -262,6 +285,11 @@ print(f"\ndose = x*cfac/v={wpdose*cfac/v}uSv/s\n")
 print(f"\ndose = (x*cfac/v)*(3.6e9)={wpdose*cfac/v}*{3600/1e6}={wpdose*cfac/v*3600/1e6}uSv/hr\n")
 print(f"\ndose = (x*cfac/v)*(3.6e9)={wpdose*cfac/v}*{60/1e10}={wpdose*cfac/v*60/1e10}MU/min\n")
 
+
+
+
+
+"""
 s_rate=mu*volume_wp/wpdose
 
 def rounde(unrounded):
@@ -279,3 +307,4 @@ print(f"k = {k}")
 
 
 print(f"k smoothen = {volume_wp/y_maxs*36}")
+"""
