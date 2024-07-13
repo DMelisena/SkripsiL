a2796a1216bc60dec10d4d694bc7202ed4970d56
DMelisena <demonlordx777@gmail.com>
2024-07-14 06:32:16 +0700

    Adding version checkers

---
+++ 2simulasi/Kalibrasi/newkal/locate_openmc.py
@@ -0,0 +1,2 @@
+import openmc
+import.plot_geometry(False,"/home/sena/.pyenv/versions/3.11.4/lib/python3.11/site-packages/openmc/__init__.py",cwd)
---
+++ 2simulasi/Kalibrasi/newkal/omcCheck.py
@@ -0,0 +1,7 @@
+import openmc
+print(openmc.__file__)
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+import math
+print(math.__doc__)
+
